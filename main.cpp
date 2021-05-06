407802b87abbcab08d7150982996b79d11d19eb7
3gafty <41028994+3gafty@users.noreply.github.com>
2021-05-06 22:22:29 +0300

    Update main.cpp
    1) Замена exit на исключения
    2) строка в качестве имени файла

--- main.cpp
+++ main.cpp
@@ -4,24 +4,30 @@
 
 int main(int argc, const char* argv[])
 {
-	std::cout << "start" << std::endl;
-        std::pair<sockaddr_in, std::vector<sockaddr_in>> addrsConnections;
-        int freq;
+        try
+        {
+                std::cout << "start" << std::endl;
+                std::pair<sockaddr_in, std::vector<sockaddr_in>> addrsConnections;
+                int freq;
+                std::string fileName(argv[1]);
 
-        if (argc < 2) {
-                std::cerr << "Not select config file" << std::endl;
-                exit(1);
-        }
-        else if (argc > 2) {
-                std::cerr << "To many arguments." << std::endl;
-                exit(2);
-        }
-        else {
-                addrsConnections = parsAddress(argv[1]);
-                freq = parsFreqOfUDP(argv[1]);
-        }
+                if (argc < 2) {
+                        throw std::runtime_error(std::string("Not select config file"));
+                }
+                else if (argc > 2) {
+                        throw std::runtime_error(std::string("To many arguments."));
+                }
+                else {
+                        addrsConnections = parsAddress(fileName);
+                        freq = parsFreqOfUDP(fileName);
+                }
 
-        networkLoadBalancer(addrsConnections, freq);
-
+                networkLoadBalancer(addrsConnections, freq);
+        }
+        catch(const std::exception& e)
+        {
+                std::cerr << e.what() << '\n';
+                return 1;
+        }
         return 0;
 }

6803168bb23ef13e42b786cf3a757ae862ed4150
3gafty <41028994+3gafty@users.noreply.github.com>
2021-05-06 22:26:14 +0300

    Update networkLoadBalancer.cpp
    1) исправлена логика алгоритма. Теперь поток не спит и читает до тех пор, пока не достигнется требуемое число посылок в секунду. По достижении этого значения, продолжает вычитывать, но не пресылает, пока не начнется следующая секунда. Вычитанные и не пересланые посылки теряются.
    2) доавлены исключения
    3) закоменчен оттладочный вывод
    4) добавлена работа со временем

--- networkLoadBalancer.cpp
+++ networkLoadBalancer.cpp
@@ -1,45 +1,55 @@
 #include "networkLoadBalancer.h"
 
-void networkLoadBalancer(const std::pair<sockaddr_in, std::vector<sockaddr_in>>& conn, const int freq)
+void networkLoadBalancer(const std::pair<sockaddr_in, std::vector<sockaddr_in>>& conn, const unsigned int freq)
 {
+	using namespace std::chrono;
+
         int listener = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (listener < 0) {
-		perror("socket");
-		exit(1);
+		throw std::runtime_error("socket");
 	}
 
 	int opt = 1;
 	if (setsockopt (listener, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-		perror("setsockopt");
-		exit(3);
+		throw std::runtime_error("setsockopt");
 	}
 
 	if (bind(listener, (struct sockaddr *)&conn.first, sizeof(conn.first)) < 0) {
-		perror("bind");
-		exit(4);
+		throw std::runtime_error("bind");
 	}
 
-        bool run{true};
+	bool timeFlag{false};
 	int num{0};
 	unsigned int dgCounter{0};
-	while (run) {
+	uint64_t elapsedTimer{0};
+	auto lastTimePoint = steady_clock::now();
+	while (true) {
 		char buf[SIZEBUFF];
-		sockaddr_in clientAddr = conn.second[num++];
-		num %= conn.second.size();
+		sockaddr_in clientAddr = conn.second[num];
 		socklen_t sizeAddr = sizeof(clientAddr);
 		int err = recvfrom(listener, buf, SIZEBUFF, 0, 0, 0);
-		auto timePoint = std::chrono::steady_clock::now();
+		auto timePointCurrent = steady_clock::now();
 		if (err > 0 && err < SIZEBUFF) {
 			++dgCounter;
-			buf[err] = 0;
-                        std::string output(buf);
-			std::cout << "sendto port - " << ntohs(clientAddr.sin_port) << std::endl;
-			sendto(listener, buf, err, 0, (struct sockaddr *)&clientAddr, sizeAddr);
-			std::cout << output << std::endl;
-			output.clear();
+			if (timeFlag) {
+				auto timeDur = timePointCurrent - lastTimePoint;
+				elapsedTimer += duration_cast<milliseconds>(timeDur).count();
+			}
+			if (dgCounter <= freq && elapsedTimer <= ONE_SECOND) {
+				//std::cout << "sendto port - " << ntohs(clientAddr.sin_port) << std::endl;
+				sendto(listener, buf, err, 0, (struct sockaddr *)&clientAddr, sizeAddr);
+				++num %= conn.second.size();
+			}
+			else if (elapsedTimer > ONE_SECOND) {
+				dgCounter = 0;
+				elapsedTimer = 0;
+			}
+			//std::cout << std::endl;
 		}
 		else {
 			std::cerr << "Error resieved" << std::endl;
 		}
+		timeFlag = true;
+		lastTimePoint = timePointCurrent;
 	}
 }

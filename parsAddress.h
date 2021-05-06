26a28ab149a74ed4e02cab7641349875fb559c85
3gafty <41028994+3gafty@users.noreply.github.com>
2021-05-07 00:11:18 +0300

    includes

--- parsAddress.h
+++ parsAddress.h
@@ -1,16 +1,10 @@
 #ifndef PARSADDRESS_H
 #define PARSADDRESS_H
 
-#include <iostream>
-#include <fstream>
 #include <vector>
 #include <utility>
-#include <cstring>
 #include <string>
-#include <exception>
 
-#include <unistd.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 

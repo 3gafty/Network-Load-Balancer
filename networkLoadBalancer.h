dfc9fcd04aa7d781d7fc74fe1ef9db92b2f03869
3gafty <41028994+3gafty@users.noreply.github.com>
2021-05-07 00:09:06 +0300

    includes

--- networkLoadBalancer.h
+++ networkLoadBalancer.h
@@ -1,21 +1,10 @@
 #ifndef NETWORKLOADBALANCER_H
 #define NETWORKLOADBALANCER_H
 
-#include <iostream>
-#include <fstream>
 #include <vector>
 #include <utility>
-#include <cstring>
-#include <chrono>
-#include <thread>
-#include <cmath>
-#include <exception>
 
-#include <sys/socket.h>
-#include <unistd.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <fcntl.h>
 
 #define SIZEBUFF 1024
 #define ONE_SECOND 1000

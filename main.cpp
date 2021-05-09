db924676b635987e915af8c5dac2d7401b4958bf
3gafty <41028994+3gafty@users.noreply.github.com>
2021-05-09 23:38:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include "parsAddress.h"
 #include "parsFreqOfUDP.h"
-#include "networkLoadBalancer.h"
+#include "network_load_balancer.h"
+#include "closing_message.h"
 
 #include <iostream>
 #include <csignal>
-#include <cstring>
-
-#include <sys/socket.h>
-#include <unistd.h>
-#include <netinet/in.h>
+#include <memory>
 
 void handleSignal(int signal);
 
-void closingMessage();
-
-volatile bool run{true};
+auto Nlb = std::make_unique<mynamespace::NetworkLoadBalancer>();
 
 int main(int argc, const char* argv[])
 {
@@ -23,8 +18,6 @@ int main(int argc, const char* argv[])
 	try
 	{
 		std::cout << "Start Program" << std::endl;
-		std::pair<sockaddr_in, std::vector<sockaddr_in>> addrsConnections;
-		int freq;
 
 		if (argc < 2) {
 			throw std::runtime_error(std::string("Not select config file"));
@@ -34,61 +27,39 @@ int main(int argc, const char* argv[])
 		}
 		else {
 			std::string fileName(argv[1]);
-			addrsConnections = parsAddress(fileName);
-			freq = parsFreqOfUDP(fileName);
+			Nlb->setConnections(parsAddress(fileName));
+			Nlb->setNumberOfMessagesPerSecond(parsFreqOfUDP(fileName));
 		}
 
-		networkLoadBalancer(addrsConnections, freq);
+		Nlb->run();
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << e.what() << std::endl;
 		return 1;
 	}
+	catch(...) {
+		std::cerr << "unknown exception!" << std::endl;
+		return 2;
+	}
 
-	std::cout << "Exit program" << std::endl;
+	std::cout << "Stop program" << std::endl;
 	return 0;
 }
 
 void handleSignal(int signal) {
 	switch(signal) {
 		case SIGINT:
-			run = false;
+			Nlb->stop();
 			closingMessage();
 		break;
 
 		case SIGTERM:
-			run = false;
+			Nlb->stop();
 			closingMessage();
 		break;
 
 		default:
 			std::cerr << "!unknown signal!" << std::endl;
 	}
-}
-
-void closingMessage()
-{
-	int finalizer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (finalizer < 0) {
-		std::cerr << "Socket failed. The errno value is: " + std::to_string(errno) << std::endl;
-	}
-	sockaddr_in sendAddrFinal;
-	memset(&sendAddrFinal, 0, sizeof(sendAddrFinal));
-	sendAddrFinal.sin_family = AF_INET;
-	sendAddrFinal.sin_port = htons(12345);
-	sendAddrFinal.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	int opt = 1;
-	if (setsockopt (finalizer, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-		std::cerr << "Setsockopt failde. The errno value is: " + std::to_string(errno) << std::endl;
-	}
-
-	std::string byby("exit");
-	socklen_t sizeAddr = sizeof(sendAddrFinal);
-	int senderrFinal = sendto(finalizer, &byby, byby.size() + 1, 0, (struct sockaddr *)&sendAddrFinal, sizeAddr);
-	if (senderrFinal < 0) {
-		std::cerr << "Error sendto. The errno value is : " << errno << std::endl;
-	}
-	close(finalizer);
 }

7ce02d2b5048ce5e97ed864292ef7ef4022414d8
3gafty@gmail.com <3gafty@gmail.com>
2021-08-09 23:46:51 +0300

    some progress in test, but not build

--- tests/test_nlb.cpp
+++ tests/test_nlb.cpp
@@ -1,4 +1,30 @@
+#include "sender_recipient.h"
+#include "../src/network_load_balancer.h"
+#include "../src/parser.h"
+
 #include <gtest/gtest.h>
+#include <thread>
+
+TEST (NLB, evarege_load) {
+    using namespace My_NLB;
+    try {
+        Parser prs;
+        NLB nlb(std::move(prs.getConns()), prs.getNomps());
+        uint32_t count_recipient_1{0};
+        uint32_t count_recipient_2{0};
+        uint32_t count_recipient_3{0};
+        std::thread(recipient, 12346, std::ref(count_recipient_1)).detach();
+        std::thread(recipient, 12347, std::ref(count_recipient_2)).detach();
+        std::thread(recipient, 12348, std::ref(count_recipient_3)).detach();
+        std::thread(sender, 12345).join();
+
+        ASSERT_TRUE((count_recipient_1 + count_recipient_2 + count_recipient_3) / 3 <= prs.getNomps);
+    } catch (const exception& e) {
+        std::cerr << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception!" << std::endl;
+    }
+}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
